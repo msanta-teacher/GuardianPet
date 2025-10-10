de94cf7645db35437710af24677f20eb801ad2d8
Miguel Santa <msanta@inter.edu.co>
2025-10-10 18:31:40 -0500

    fix prisma

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import userRoutes from "./modules/usuario/user.routes.js";
-import mascotaRoutes from "./modules/mascota/mascota.routes.js";
+//import mascotaRoutes from "./modules/mascota/mascota.routes.js";
 // importa resto de módulos...
 
 const r = Router();
